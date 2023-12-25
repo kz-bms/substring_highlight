584226f865b0f259e39384117c76ac3da6e84a96
GnaneshwarDG <gnaneshwar.g@bluemindz.com>
2023-12-25 10:27:12 +0530

    Text beautification impl KZSF-43/KZSB-297

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+class ColorsConst {
+
+  static const int kzRed = 0xffE4007E;
+  static const int kzBlue = 0xff0069B4;
+  static const int kzDarkGrey = 0xff354042;
+  static const int kzCyan = 0xff0BBBEF;
+  static const int kzGreen = 0xff13A538;
+  static const int kzGrey = 0xff8C8F91;
+  static const int kzWhite = 0xffffffff;
+  static const int kzWhite90 = 0xE6ffffff;
+  static const int kzCyan35 = 0x590BBBEF;
+  static const int kzGreen20 = 0x3313A538;
+  static const int kzGreen8 = 0x1413A538;
+}
--- lib/substring_highlight.dart
+++ lib/substring_highlight.dart
@@ -6,6 +6,8 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'colors.dart';
+
 final int __int64MaxValue = double.maxFinite.toInt();
 
 /// Widget that renders a string with sub-string highlighting.
@@ -73,22 +75,35 @@ class SubstringHighlight extends StatelessWidget {
       r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])');
 
   Map<String, TextStyle> textStylePattern = {
-    r'_(.*?)\_': const TextStyle(
-      fontStyle: FontStyle.italic,
-    ),
-    r'\*(.*?)\*': const TextStyle(
+
+    r'(^\*|(?<=\s)\*)[^ \t](.*?)[^ \t]+\*': const TextStyle(
       fontWeight: FontWeight.bold,
     ),
-    r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+|[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-zA-Z]{2,7})':
-        const TextStyle(
-            color: Colors.blueAccent, decoration: TextDecoration.underline),
-    /* r'\~(.*?)\~': const TextStyle(
-      decoration: TextDecoration.lineThrough,
-    ),*/
+    r'(^\_|(?<=\s)\_)[^ \t](.*?)[^ \t]+\_': const TextStyle(
+      fontStyle: FontStyle.italic,
+    ),
+    r'(^\-|(?<=\s)\-)[^ \t](.*?)[^ \t]+\-': const TextStyle(
+      decoration: TextDecoration.underline,
+    ),
+    r'(^\#|(?<=\s)\#)[^ \t](.*?)[^ \t]+\#': const TextStyle(
+      color: Color(ColorsConst.kzRed),
+    ),
+    r'(^\%|(?<=\s)\%)[^ \t](.*?)[^ \t]+\%': const TextStyle(
+      color: Color(ColorsConst.kzGreen),
+    ),
+    r'(^\!|(?<=\s)\!)[^ \t](.*?)[^ \t]+\!': const TextStyle(
+      color: Color(ColorsConst.kzBlue),
+    )
   };
 
-  bool isBold = false;
-  bool isItalic = false;
+  RegExp fileTypeRegex = RegExp(
+      '^.*\.(jpg|jpeg|png|gif|raw|tiff|psd|doc|docx|html|css|xls|xlsx|ppt|pptx|zip|pdf|txt|opus|mp3|mp4|mov|avi|m4a|wav)\$',
+      caseSensitive: false);
+
+  final urlRegex = RegExp(
+      r'''^(.*?)((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//="'`]*))''',
+      caseSensitive: false,
+      dotAll: true);
 
   @override
   Widget build(BuildContext context) {
@@ -111,8 +126,7 @@ class SubstringHighlight extends StatelessWidget {
       // print('=== idx=$idx');
 
       nonHighlightAdd(int end) {
-        return children
-            .add(TextSpan(children: getTextStyle(text.substring(start, end))));
+        return children.add(getTextWidget(text: text.substring(start, end)));
       }
 
       // find index of term that's closest to current idx position
@@ -194,18 +208,12 @@ class SubstringHighlight extends StatelessWidget {
         textScaleFactor: MediaQuery.of(context).textScaleFactor);
   }
 
-  List<InlineSpan> getTextStyle(String text) {
-    List<InlineSpan> child = [];
-    final urlRegex = RegExp(
-        r'(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+|[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-zA-Z]{2,7})',
-        caseSensitive: false,
-        dotAll: true);
-/*
-    RegExp(
-      r'^(.*?)((?:https?:\/\/|www\.)[^\s/$.?#].[^\s]*)',
-      caseSensitive: false,
-      dotAll: true,
-    );*/
+  TextSpan getTextWidget({
+    required String text,
+    TextStyle? style,
+  }) {
+    TextStyle? textStyle = const TextStyle();
+    List<TextSpan> child = [];
 
     var pattern = RegExp(
         textStylePattern.keys.map((key) {
@@ -216,142 +224,127 @@ class SubstringHighlight extends StatelessWidget {
     text.splitMapJoin(
       pattern,
       onMatch: (Match match) {
-        for (var text in match.group(0)!.toString().split(" ")) {
-          text = text + " ";
-          var urlMatch = urlRegex.firstMatch(text);
-          if (urlMatch != null) {
-            child.add(TextSpan(
-              text: urlMatch.group(0),
-              style: TextStyle(
-                  decoration: TextDecoration.underline, color: Colors.blue),
-              recognizer: TapGestureRecognizer()
-                ..onTap = () async {
-                  Uri url = Uri.parse(urlMatch.group(0).toString());
-                  if (await canLaunchUrl(url)) {
-                    await launchUrl(url, mode: LaunchMode.externalApplication);
-                  } else if (urlMatch.group(0)?.contains('@') ?? false) {
-                    // Handle email address here
-                    String email = urlMatch.group(0).toString();
-                    final Uri emailUrl = Uri(
-                      scheme: 'mailto',
-                      path: email,
-                    );
-                    await launchUrl(emailUrl);
-                  } else {
-                    await launchUrl(Uri.parse('https://$url'),
-                        mode: LaunchMode.externalApplication);
-                  }
-                },
-            ));
-          } else {
-            for (var char in text.toString().characters) {
-              if (char == '*') {
-                isBold = !isBold;
-                child.add(TextSpan(
-                    text: "",
-                    style: TextStyle(
-                        fontSize:
-                            regexEmoji.allMatches(char).isNotEmpty ? 18 : 14,
-                        fontWeight:
-                            isBold ? FontWeight.bold : FontWeight.normal,
-                        fontStyle:
-                            isItalic ? FontStyle.italic : FontStyle.normal,
-                        color: Colors.black,
-                        fontFamily: Platform.isIOS
-                            ? regexEmoji.allMatches(char).isNotEmpty
-                                ? 'Apple Color Emoji'
-                                : ''
-                            : '')));
-              } else if (char == '_') {
-                isItalic = !isItalic;
-                child.add(TextSpan(
-                    text: "",
-                    style: TextStyle(
-                        fontSize:
-                            regexEmoji.allMatches(char).isNotEmpty ? 18 : 14,
-                        color: Colors.black,
-                        fontWeight:
-                            isBold ? FontWeight.bold : FontWeight.normal,
-                        fontStyle:
-                            isItalic ? FontStyle.italic : FontStyle.normal,
-                        fontFamily: Platform.isIOS
-                            ? regexEmoji.allMatches(char).isNotEmpty
-                                ? 'Apple Color Emoji'
-                                : ''
-                            : '')));
-              } else {
-                child.add(TextSpan(
-                    text: char,
-                    style: TextStyle(
-                        fontSize:
-                            regexEmoji.allMatches(char).isNotEmpty ? 18 : 14,
-                        color: Colors.black,
-                        fontWeight:
-                            isBold ? FontWeight.bold : FontWeight.normal,
-                        fontStyle:
-                            isItalic ? FontStyle.italic : FontStyle.normal,
-                        fontFamily: Platform.isIOS
-                            ? regexEmoji.allMatches(char).isNotEmpty
-                                ? 'Apple Color Emoji'
-                                : ''
-                            : '')));
-              }
-            }
-          }
-        }
+        String matchedText = match.group(0).toString();
+
+        TextStyle? textStyle =
+        textStylePattern[textStylePattern.keys.firstWhere((e) {
+          bool hasMatch = false;
+          RegExp(e).allMatches(matchedText).forEach((element) {
+            if (element.group(0) == match.group(0)) hasMatch = true;
+          });
+          return hasMatch;
+        })];
+
+        String inputText = matchedText.substring(1, matchedText.length - 1);
+
+        child.add(RegExp(textStylePattern.keys.map((e) => e).join('|'))
+                .hasMatch(inputText)
+            ? getTextWidget(
+                text: inputText,
+                style: textStyle != null ? textStyle?.merge(style) : style,
+              )
+            : TextSpan(
+                children: getUrlTextWidget(
+                text: inputText,
+                style: textStyle != null
+                    ? textStyle?.merge(style)
+                    : const TextStyle().merge(style),
+              )));
+
         return "";
       },
       onNonMatch: (String text) {
-        for (var textElement in text.split(" ")) {
-          textElement = textElement + " ";
-          var urlMatch = urlRegex.firstMatch(textElement);
-          if (urlMatch != null) {
-            child.add(TextSpan(
-              text: urlMatch.group(0),
-              style: TextStyle(
-                  decoration: TextDecoration.underline, color: Colors.blue),
-              recognizer: TapGestureRecognizer()
-                ..onTap = () async {
-                  Uri url = Uri.parse(urlMatch.group(0).toString());
-                  if (await canLaunchUrl(url)) {
-                    await launchUrl(url, mode: LaunchMode.externalApplication);
-                  } else if (urlMatch.group(0)?.contains('@') ?? false) {
-                    // Handle email address here
-                    String email = urlMatch.group(0).toString();
-                    final Uri emailUrl = Uri(
-                      scheme: 'mailto',
-                      path: email,
-                    );
-                    await launchUrl(emailUrl);
-                  } else {
-                    await launchUrl(Uri.parse('https://$url'),
-                        mode: LaunchMode.externalApplication);
-                  }
-                },
-            ));
-          } else {
-            for (String t in textElement.characters) {
-              child.add(TextSpan(
-                  text: (term ?? '').isEmpty
-                      ? t
-                      : RegExp(r'[*_]+').allMatches(t).isNotEmpty
-                          ? " "
-                          : t,
-                  style: TextStyle(
-                      fontSize: regexEmoji.allMatches(t).isNotEmpty ? 18 : 14,
-                      color: Colors.black,
-                      fontFamily: Platform.isIOS
-                          ? regexEmoji.allMatches(t).isNotEmpty
-                              ? 'Apple Color Emoji'
-                              : ''
-                          : '')));
-            }
-          }
+        if (text.isNotEmpty) {
+          child.add(TextSpan(
+              children: getUrlTextWidget(
+                text: text,
+                style: style != null ? style.merge(textStyle) : TextStyle(),
+              )));
         }
         return "";
       },
     );
 
-    return child;
+    return TextSpan(children: child);
+  }
+
+
+  List<TextSpan> getUrlTextWidget({
+    required String text,
+    TextStyle? style,
+  }) {
+    List<TextSpan> span = [];
+
+    text.splitMapJoin(urlRegex, onMatch: (Match match) {
+      var fileMatch = fileTypeRegex.firstMatch(match.group(0).toString());
+      if (fileMatch != null) {
+        span.add(TextSpan(
+            children: getTextStyleWidget(
+              text,
+              style: style ?? const TextStyle(),
+            )));
+      } else {
+        span.add(TextSpan(
+          text: match.group(0).toString(),
+          style: style != null
+              ? style.merge(TextStyle(
+              decoration: TextDecoration.underline, color: Colors.blue))
+              : TextStyle(
+              decoration: TextDecoration.underline, color: Colors.blue),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () async {
+              Uri url = Uri.parse(text);
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url, mode: LaunchMode.externalApplication);
+              } else if (text.contains('@')) {
+                // Handle email address here
+                String email = text;
+                final Uri emailUrl = Uri(
+                  scheme: 'mailto',
+                  path: email,
+                );
+                await launchUrl(emailUrl);
+              } else {
+                await launchUrl(Uri.parse('https://$url'),
+                    mode: LaunchMode.externalApplication);
+              }
+            },
+        ));
+      }
+      return '';
+    }, onNonMatch: (String text) {
+      span.add(TextSpan(
+          children: getTextStyleWidget(
+            text,
+            style: style ?? const TextStyle(),
+          )));
+      return '';
+    });
+
+    return span;
+  }
+
+  List<TextSpan> getTextStyleWidget(
+    String text, {
+    required TextStyle style,
+  }) {
+    List<TextSpan> span = [];
+    for (var char in text.characters) {
+      span.add(TextSpan(
+          text: (term ?? '').isEmpty
+              ? char
+              : RegExp(r'[\*\_\-\#\%\!]+').allMatches(char).isNotEmpty
+                  ? ' '
+                  : char,
+          style: style.merge(TextStyle(
+              fontSize: regexEmoji.allMatches(char).isNotEmpty ? 18 : 14,
+              color: style.color ?? Colors.black,
+              fontFamily: Platform.isIOS
+                  ? regexEmoji.allMatches(char).isNotEmpty
+                      ? 'Apple Color Emoji'
+                      : ''
+                  : ''))));
+    }
+    return span;
   }
 }
