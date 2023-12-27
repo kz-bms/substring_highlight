d285adaf6aa2d8cc2c588a41186a17d4852c8991
GnaneshwarDG <gnaneshwar.g@bluemindz.com>
2023-12-27 11:40:45 +0530

    Changed Regex to match url

--- lib/substring_highlight.dart
+++ lib/substring_highlight.dart
@@ -101,7 +101,7 @@ class SubstringHighlight extends StatelessWidget {
       caseSensitive: false);
 
   final urlRegex = RegExp(
-      r'''^(.*?)((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//="'`]*))''',
+      r'''((https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//="'`]*))''',
       caseSensitive: false,
       dotAll: true);
 
